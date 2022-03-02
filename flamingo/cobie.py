4cfe2c49d3846eb5865d3c0b7337041ff6b93f20
Michael Pfammatter <pfammatter@gmail.com>
2022-03-02 14:25:49 -0600

    COBie: Add CDX Space assignment functions

--- flamingo/cobie.py
+++ flamingo/cobie.py
@@ -6,9 +6,211 @@ from flamingo.revit import GetSchedulableFields
 from pyrevit import forms, HOST_APP, revit, script
 import re
 import System
-from datetime import datetime
 from xml.etree import ElementTree as ET
 
+
+CDX_PARAMETER_MAP = [
+    {
+        "type": "Facility",
+        "cobie": "COBie.Facility.Name",
+        "cdx": "GSA.01.Facility.GSABuildingCode",
+        "guid": "7704D27B-55E1-4E0D-A323-BE403AEE4CDD",
+    },
+    {
+        "type": "Facility",
+        "cobie": "COBie.Facility.Category",
+        "cdx": "GSA.01.Facility.OmniClass.FacilityCategory",
+        "guid": "A0A94036-DCE0-45DC-B25B-B19632CEEA23",
+    },
+    {
+        "type": "Facility",
+        "cobie": "COBie.Facility.ProjectName",
+        "cdx": "GSA.01.Facility.ProjectName",
+        "guid": "D18B5EA9-0C02-45A4-ACD6-D8887A5BBAD4",
+    },
+    {
+        "type": "Facility",
+        "cobie": "COBie.Facility.SiteName",
+        "cdx": "GSA.01.Facility.SiteName",
+        "guid": "52E4EF7A-08FB-44C5-8935-66BA1C2EE94B",
+    },
+    {
+        "type": "Floor",
+        "cobie": "COBie.Floor.Name",
+        "cdx": "GSA.01.Floor.Name",
+        "guid": "1BB202D3-D9E2-428A-8375-EE6D01A98FFA",
+    },
+    {
+        "type": "Floor",
+        "cobie": "COBie.Floor.Category",
+        "cdx": "GSA.01.Floor.FloorCategory",
+        "guid": "8836ACFF-EA52-41FE-BC49-8E81D1CD8605",
+    },
+    {
+        "type": "Space",
+        "cobie": "COBie.Space.Name",
+        "cdx": "GSA.02.Space.RoomNumber",
+        "guid": "57E67856-3996-4DC3-AD75-4C33A76704A0",
+    },
+    {
+        "type": "Space",
+        "cobie": "COBie.Space.Category",
+        "cdx": "GSA.03.Space.OmniClass.SpaceCategory",
+        "guid": "00DCCF44-54E8-4290-B386-2C6787C06376",
+    },
+    {
+        "type": "Space",
+        "cobie": DB.BuiltInParameter.LEVEL_NAME,
+        "cdx": "GSA.03.Space.FloorName",
+        "guid": "FC13D9B2-31FE-4B58-A19D-F679C3F641CD",
+    },
+    {
+        "type": "Space",
+        "cobie": "COBie.Space.Description",
+        "cdx": "GSA.03.Space.Description",
+        "guid": "DF8F1C12-F287-4360-AFBC-CC92B0B56D2A",
+    },
+    {
+        "type": "Space",
+        "cobie": "COBie.Space.RoomTag",
+        "cdx": "GSA.05.Space.Signage",
+        "guid": "28ECE7CD-8818-436D-AF6C-733D3AE0E69B",
+    },
+    {
+        "type": "Space",
+        "cobie": "COBie.Zone.Name",
+        "cdx": "GSA.03.Space.ZoneName",
+        "guid": "B5DB0243-5AFE-4153-B037-775E21CC57F1",
+    },
+    {
+        "type": "Space",
+        "cobie": DB.BuiltInParameter.ROOM_AREA,
+        "cdx": "GSA.02.Space.ANSIBOMA.UsableSFCalculation",
+        "guid": "C4BF9EEB-6A71-4BFD-B4E9-AF88D4302B25",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.Name",
+        "cdx": "GSA.05.Asset.TypeName",
+        "guid": "0798BDCD-761D-4942-8FF2-0E7007F40E6A",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.Category",
+        "cdx": "GSA.05.Asset.AssetType.Abbreviation",
+        "guid": "37791872-234C-4766-9F31-7A3E72050582",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.Description",
+        "cdx": "GSA.05.Asset.AssetType.Description",
+        "guid": "ACD08F9A-6EAB-4981-8388-14A806DB7DFF",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.AssetType",
+        "cdx": "GSA.05.Asset.Mobility",
+        "guid": "F5A7AEB6-2216-45BA-B85F-C9E75201F9B2",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.Manufacturer",
+        "cdx": "GSA.07.Asset.Manufacturer.Email",
+        "guid": "639F8842-58BF-48C9-9B00-8C4D2790BA68",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.ModelNumber",
+        "cdx": "GSA.07.Asset.Model.Number",
+        "guid": "C20C3645-948F-4846-B80E-93049348FA0C",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.WarrantyDurationParts",
+        "cdx": "GSA.07.Asset.WarrantyDuration.Parts",
+        "guid": "4469578B-15B2-46DF-BA68-45BFA7D53AF7",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.WarrantyDurationLabor",
+        "cdx": "GSA.07.Asset.WarrantyDuration.Labor",
+        "guid": "C9021772-A5C2-471D-B116-3BD710D0FE31",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.WarrantyDurationUnit",
+        "cdx": "GSA.07.Asset.WarrantyDuration.Unit",
+        "guid": "4C4306D4-927C-4CD5-8501-6D58096AC4A3",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.ExpectedLife",
+        "cdx": "GSA.07.Asset.ExpectedLife",
+        "guid": "73F20796-C594-4929-A364-F34CBEB0CCD7",
+    },
+    {
+        "type": "Type",
+        "cobie": "COBie.Type.DurationUnit",
+        "cdx": "GSA.07.Asset.ExpectedLife.Unit",
+        "guid": "E70826DF-0EFA-4F2B-96FA-682DB5E6CDEA",
+    },
+    {
+        "type": "Component",
+        "cobie": "COBie.Component.Name",
+        "cdx": "GSA.06.Asset.InstanceName",
+        "guid": "E21B3065-6702-4973-9E1F-E3F21EF27581",
+    },
+    {
+        "type": "Component",
+        "cobie": "COBie.Component.Space",
+        "cdx": "GSA.06.Asset.SpaceCode",
+        "guid": "E2D4398B-F79D-486A-9B23-81614ED9AA84",
+    },
+    {
+        "type": "Component",
+        "cobie": "COBie.Component.Description",
+        "cdx": "GSA.05.Asset.Description",
+        "guid": "434BE294-6B1A-4B85-A30E-A2986B2AF718",
+    },
+    {
+        "type": "Component",
+        "cobie": "COBie.Component.SerialNumber",
+        "cdx": "GSA.09.Asset.SerialNumber",
+        "guid": "E700395C-92AA-4AE8-86A5-5F3EDC67C944",
+    },
+    {
+        "type": "Component",
+        "cobie": "COBie.Component.InstallationDate",
+        "cdx": "GSA.09.Asset.InstallationDate",
+        "guid": "AB317582-D3F4-4437-B9BC-2671B0965AD4",
+    },
+    {
+        "type": "Component",
+        "cobie": "COBie.Component.WarrantyStartDate",
+        "cdx": "GSA.09.Asset.WarrantyStartDate",
+        "guid": "B46565AA-C5A7-4E6E-AC31-5A2D291846BC",
+    },
+    {
+        "type": "Asset",
+        "cobie": "COBie.System.Name",
+        "cdx": "GSA.06.Asset.SystemName",
+        "guid": "C9711AA6-0A1C-4299-9A46-429F9D6E3517",
+    },
+    {
+        "type": "Asset",
+        "cobie": "COBie.System.Category",
+        "cdx": "GSA.06.Asset.OmniClass.SystemCategory",
+        "guid": "0CECE43C-75DB-4EFE-85BE-90E4056AB09",
+    },
+]
+
+CDX_ANSI_BOMA_CODE_MAP = {
+    "Office": "01",
+    "Building Common": "02",
+    "Floor Common": "03",
+    "Vertical Penetration": "04",
+    "PBS Specific": "05"
+}
 
 def SetCOBieParameter(element, parameterName, value, blankOnly=False):
     """Set a value for a COBie parameter. The parameter must be a string
@@ -440,202 +642,6 @@ def SetCDXParameterFromCOBie(element, cdxGuid, COBieParameterName):
     return cdxParameter
 
 
-CDX_PARAMETER_MAP = [
-    {
-        "type": "Facility",
-        "cobie": "COBie.Facility.Name",
-        "cdx": "GSA.01.Facility.GSABuildingCode",
-        "guid": "7704D27B-55E1-4E0D-A323-BE403AEE4CDD",
-    },
-    {
-        "type": "Facility",
-        "cobie": "COBie.Facility.Category",
-        "cdx": "GSA.01.Facility.OmniClass.FacilityCategory",
-        "guid": "A0A94036-DCE0-45DC-B25B-B19632CEEA23",
-    },
-    {
-        "type": "Facility",
-        "cobie": "COBie.Facility.ProjectName",
-        "cdx": "GSA.01.Facility.ProjectName",
-        "guid": "D18B5EA9-0C02-45A4-ACD6-D8887A5BBAD4",
-    },
-    {
-        "type": "Facility",
-        "cobie": "COBie.Facility.SiteName",
-        "cdx": "GSA.01.Facility.SiteName",
-        "guid": "52E4EF7A-08FB-44C5-8935-66BA1C2EE94B",
-    },
-    {
-        "type": "Floor",
-        "cobie": "COBie.Floor.Name",
-        "cdx": "GSA.01.Floor.Name",
-        "guid": "1BB202D3-D9E2-428A-8375-EE6D01A98FFA",
-    },
-    {
-        "type": "Floor",
-        "cobie": "COBie.Floor.Category",
-        "cdx": "GSA.01.Floor.FloorCategory",
-        "guid": "8836ACFF-EA52-41FE-BC49-8E81D1CD8605",
-    },
-    {
-        "type": "Space",
-        "cobie": "COBie.Space.Name",
-        "cdx": "GSA.02.Space.RoomNumber",
-        "guid": "57E67856-3996-4DC3-AD75-4C33A76704A0",
-    },
-    {
-        "type": "Space",
-        "cobie": "COBie.Space.Category",
-        "cdx": "GSA.03.Space.OmniClass.SpaceCategory",
-        "guid": "00DCCF44-54E8-4290-B386-2C6787C06376",
-    },
-    {
-        "type": "Space",
-        "cobie": DB.BuiltInParameter.LEVEL_NAME,
-        "cdx": "GSA.03.Space.FloorName",
-        "guid": "FC13D9B2-31FE-4B58-A19D-F679C3F641CD",
-    },
-    {
-        "type": "Space",
-        "cobie": "COBie.Space.Description",
-        "cdx": "GSA.03.Space.Description",
-        "guid": "DF8F1C12-F287-4360-AFBC-CC92B0B56D2A",
-    },
-    {
-        "type": "Space",
-        "cobie": "COBie.Space.RoomTag",
-        "cdx": "GSA.05.Space.Signage",
-        "guid": "28ECE7CD-8818-436D-AF6C-733D3AE0E69B",
-    },
-    {
-        "type": "Space",
-        "cobie": "COBie.Zone.Name",
-        "cdx": "GSA.03.Space.ZoneName",
-        "guid": "B5DB0243-5AFE-4153-B037-775E21CC57F1",
-    },
-    {
-        "type": "Space",
-        "cobie": DB.BuiltInParameter.ROOM_AREA,
-        "cdx": "GSA.02.Space.ANSIBOMA.UsableSFCalculation",
-        "guid": "C4BF9EEB-6A71-4BFD-B4E9-AF88D4302B25",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.Name",
-        "cdx": "GSA.05.Asset.TypeName",
-        "guid": "0798BDCD-761D-4942-8FF2-0E7007F40E6A",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.Category",
-        "cdx": "GSA.05.Asset.AssetType.Abbreviation",
-        "guid": "37791872-234C-4766-9F31-7A3E72050582",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.Description",
-        "cdx": "GSA.05.Asset.AssetType.Description",
-        "guid": "ACD08F9A-6EAB-4981-8388-14A806DB7DFF",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.AssetType",
-        "cdx": "GSA.05.Asset.Mobility",
-        "guid": "F5A7AEB6-2216-45BA-B85F-C9E75201F9B2",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.Manufacturer",
-        "cdx": "GSA.07.Asset.Manufacturer.Email",
-        "guid": "639F8842-58BF-48C9-9B00-8C4D2790BA68",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.ModelNumber",
-        "cdx": "GSA.07.Asset.Model.Number",
-        "guid": "C20C3645-948F-4846-B80E-93049348FA0C",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.WarrantyDurationParts",
-        "cdx": "GSA.07.Asset.WarrantyDuration.Parts",
-        "guid": "4469578B-15B2-46DF-BA68-45BFA7D53AF7",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.WarrantyDurationLabor",
-        "cdx": "GSA.07.Asset.WarrantyDuration.Labor",
-        "guid": "C9021772-A5C2-471D-B116-3BD710D0FE31",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.WarrantyDurationUnit",
-        "cdx": "GSA.07.Asset.WarrantyDuration.Unit",
-        "guid": "4C4306D4-927C-4CD5-8501-6D58096AC4A3",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.ExpectedLife",
-        "cdx": "GSA.07.Asset.ExpectedLife",
-        "guid": "73F20796-C594-4929-A364-F34CBEB0CCD7",
-    },
-    {
-        "type": "Type",
-        "cobie": "COBie.Type.DurationUnit",
-        "cdx": "GSA.07.Asset.ExpectedLife.Unit",
-        "guid": "E70826DF-0EFA-4F2B-96FA-682DB5E6CDEA",
-    },
-    {
-        "type": "Component",
-        "cobie": "COBie.Component.Name",
-        "cdx": "GSA.06.Asset.InstanceName",
-        "guid": "E21B3065-6702-4973-9E1F-E3F21EF27581",
-    },
-    {
-        "type": "Component",
-        "cobie": "COBie.Component.Space",
-        "cdx": "GSA.06.Asset.SpaceCode",
-        "guid": "E2D4398B-F79D-486A-9B23-81614ED9AA84",
-    },
-    {
-        "type": "Component",
-        "cobie": "COBie.Component.Description",
-        "cdx": "GSA.05.Asset.Description",
-        "guid": "434BE294-6B1A-4B85-A30E-A2986B2AF718",
-    },
-    {
-        "type": "Component",
-        "cobie": "COBie.Component.SerialNumber",
-        "cdx": "GSA.09.Asset.SerialNumber",
-        "guid": "E700395C-92AA-4AE8-86A5-5F3EDC67C944",
-    },
-    {
-        "type": "Component",
-        "cobie": "COBie.Component.InstallationDate",
-        "cdx": "GSA.09.Asset.InstallationDate",
-        "guid": "AB317582-D3F4-4437-B9BC-2671B0965AD4",
-    },
-    {
-        "type": "Component",
-        "cobie": "COBie.Component.WarrantyStartDate",
-        "cdx": "GSA.09.Asset.WarrantyStartDate",
-        "guid": "B46565AA-C5A7-4E6E-AC31-5A2D291846BC",
-    },
-    {
-        "type": "Asset",
-        "cobie": "COBie.System.Name",
-        "cdx": "GSA.06.Asset.SystemName",
-        "guid": "C9711AA6-0A1C-4299-9A46-429F9D6E3517",
-    },
-    {
-        "type": "Asset",
-        "cobie": "COBie.System.Category",
-        "cdx": "GSA.06.Asset.OmniClass.SystemCategory",
-        "guid": "0CECE43C-75DB-4EFE-85BE-90E4056AB09",
-    },
-]
-
-
 def COBieParametersToCDX(doc=None):
     doc = doc or HOST_APP.doc
 
@@ -765,8 +771,85 @@ def COBieParametersToCDX(doc=None):
                             COBieParameterName=cobieName,
                         )
 
-
         return
+
+
+def GetCDXSpaceCUIValues(doc=None):
+    doc = doc or HOST_APP.doc
+    cuiData = []
+
+    rooms = (
+        DB.FilteredElementCollector(doc)
+        .OfCategory(DB.BuiltInCategory.OST_Rooms)
+        .WhereElementIsNotElementType()
+        .ToElements()
+    )
+
+    for room in rooms:
+        if COBieIsEnabled(room):
+            cuiParameter = room.LookupParameter("GSA.05.Space.CUI-SBU")
+            if cuiParameter:
+                cuiData.append(
+                    {
+                        "roomElement": room,
+                        "cuiValue": cuiParameter.AsString(),
+                        "roomNumber": room.Number,
+                        "roomName": revit.query.get_name(room),
+                    }
+                )
+    return cuiData
+
+
+def SetCDXSpaceCUIValue(room, value):
+    cuiParameter = room.LookupParameter("GSA.05.Space.CUI-SBU")
+    if cuiParameter:
+        cuiParameter.Set(value)
+    return room
+
+
+def GetCDXSpaceBOMAValues(doc=None):
+    doc = doc or HOST_APP.doc
+    bomaData = []
+
+    rooms = (
+        DB.FilteredElementCollector(doc)
+        .OfCategory(DB.BuiltInCategory.OST_Rooms)
+        .WhereElementIsNotElementType()
+        .ToElements()
+    )
+
+    for room in rooms:
+        if COBieIsEnabled(room):
+            bomaNameParameter = room.get_Parameter(
+                System.Guid("B0C2E838-BE22-44C4-8B0D-F9FAC2D9DAFD")
+            )
+            bomaCodeParameter = room.get_Parameter(
+                System.Guid("EE6CECDF-20AA-4A8A-BC49-69C2E230BE52")
+            )
+            if bomaNameParameter and bomaCodeParameter:
+                bomaData.append(
+                    {
+                        "roomElement": room,
+                        "roomNumber": room.Number,
+                        "roomName": revit.query.get_name(room),
+                        "bomaName": bomaNameParameter.AsString(),
+                        "bomaCode": bomaCodeParameter.AsString()
+                    }
+                )
+    return bomaData
+
+def SetCDXSpaceBOMAValue(room, ansi_boma_name, ansi_boma_code_map=None):
+    ansi_boma_code_map = ansi_boma_code_map or CDX_ANSI_BOMA_CODE_MAP
+    bomaNameParameter = room.get_Parameter(
+        System.Guid("B0C2E838-BE22-44C4-8B0D-F9FAC2D9DAFD")
+    )
+    bomaCodeParameter = room.get_Parameter(
+        System.Guid("EE6CECDF-20AA-4A8A-BC49-69C2E230BE52")
+    )
+    if bomaNameParameter and bomaCodeParameter:
+        bomaNameParameter.Set(ansi_boma_name)
+        bomaCodeParameter.Set(ansi_boma_code_map[ansi_boma_name])
+    return room
 
 
 def CreateScheduleView(viewName, category, parameterGUIDList=None, doc=None):
