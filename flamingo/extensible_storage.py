11ca1acb0024ed0bc9ae7a6affa4bb88a01420d4
Michael Pfammatter <pfammatter@gmail.com>
2026-01-07 15:52:14 -0600

    Create schema for settings storage

--- flamingo/extensible_storage.py
+++ flamingo/extensible_storage.py
@@ -1,7 +1,15 @@
 from Autodesk.Revit import DB
+from flamingo import FLAMINGO_VERSION
 import json
-from pyrevit import HOST_APP
+from pyrevit import HOST_APP, script
 import System
+from System import String
+from System.Collections.Generic import Dictionary, IDictionary
+
+LOGGER = script.get_logger()
+
+FLAMINGO_SCHEMA_GUID = "476690c0-e882-4b36-90a1-1c1261f33ba4"
+
 
 def GetSchemaByName(schemaName, doc=None):
     doc = doc or HOST_APP.doc
@@ -12,7 +20,8 @@ def GetSchemaByName(schemaName, doc=None):
             schemas.append(schema)
     return schemas
 
-def CreateSchemaBuilder(schemaGuid, schemaName, doc=None):
+
+def CreateSchemaBuilder(schemaGuid, schemaName, vendorId, doc=None):
     doc = doc or HOST_APP.doc
     if not type(schemaGuid) is System.Guid:
         schemaGuid = System.Guid(schemaGuid)
@@ -21,8 +30,10 @@ def CreateSchemaBuilder(schemaGuid, schemaName, doc=None):
     schemaBuilder.SetReadAccessLevel(public)
     schemaBuilder.SetWriteAccessLevel(public)
     schemaBuilder.SetSchemaName(schemaName)
+    schemaBuilder.SetVendorId(vendorId)
 
     return schemaBuilder
+
 
 def SetSchemaData(schema, fieldName, element, data):
     entity = DB.ExtensibleStorage.Entity(schema)
@@ -33,5 +44,61 @@ def SetSchemaData(schema, fieldName, element, data):
     data = retrievedEntity.Get[str](field)
     return data
 
-def GetSchemaData():
-    return
+
+def SetSchemaMapData(schema, fieldName, element, dictionary):
+    entity = DB.ExtensibleStorage.Entity(schema)
+    field = schema.GetField(fieldName)
+    entity.Set[IDictionary[String, String]](field, dictionary)
+    element.SetEntity(entity)
+    data = entity.Get[IDictionary[String, String]](field)
+    return data
+
+def GetSchemaMapData(schema, fieldName, element):
+    entity = element.GetEntity(schema)
+    data = entity.Get[IDictionary[String, String]](schema.GetField(fieldName))
+    return data
+
+def GetFlamingoSchema(doc=None):
+    doc = doc or HOST_APP.doc
+    flamingoSchema = DB.ExtensibleStorage.Schema.Lookup(
+        System.Guid(FLAMINGO_SCHEMA_GUID)
+    )
+    if flamingoSchema:
+        LOGGER.debug("Returning existing Flamingo schema.")
+        return flamingoSchema
+    LOGGER.debug("Creating new Flamingo schema.")
+    schemaBuilder = CreateSchemaBuilder(
+        FLAMINGO_SCHEMA_GUID, "Flamingo", "Flamingo", doc
+    )
+    fieldBuilder = schemaBuilder.AddMapField("Settings", System.String, System.String)
+    fieldBuilder.SetDocumentation("JSon string that includes a dictionary settings.")
+    fieldBuilder = schemaBuilder.AddSimpleField("Version", System.String)
+    fieldBuilder.SetDocumentation("Flamingo version information")
+    flamingoSchema = schemaBuilder.Finish()
+    entity = DB.ExtensibleStorage.Entity(flamingoSchema)
+    entity.Set(flamingoSchema.GetField("Version"), FLAMINGO_VERSION)
+    doc.ProjectInformation.SetEntity(entity)
+    return flamingoSchema
+
+def GetFlamingoSetting(settingKey, default=None, doc=None):
+    doc = doc or HOST_APP.doc
+    schema = GetFlamingoSchema(doc)
+    projectInfo = doc.ProjectInformation
+    entity = projectInfo.GetEntity(schema)
+    settings = entity.Get[IDictionary[String, String]](schema.GetField("Settings"))
+    if settingKey in settings:
+        return settings[settingKey]
+    else:
+        return default
+
+def SetFlamingoSetting(settingKey, settingValue, doc=None):
+    doc = doc or HOST_APP.doc
+    schema = GetFlamingoSchema(doc)
+    projectInfo = doc.ProjectInformation
+    entity = projectInfo.GetEntity(schema)
+    settings = entity.Get[IDictionary[String, String]](schema.GetField("Settings"))
+    if settings is None:
+        settings = Dictionary[String, String]()
+    settings[settingKey] = settingValue
+    entity.Set[IDictionary[String, String]](schema.GetField("Settings"), settings)
+    projectInfo.SetEntity(entity)
