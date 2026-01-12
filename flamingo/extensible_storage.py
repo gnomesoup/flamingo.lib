5dcd9fc72c79eab63be41be3afadc2252340a6c3
Michael Pfammatter <pfammatter@gmail.com>
2026-01-12 09:25:11 -0600

    Debug extensible_storage

--- flamingo/extensible_storage.py
+++ flamingo/extensible_storage.py
@@ -2,6 +2,7 @@ from Autodesk.Revit import DB
 from flamingo import FLAMINGO_VERSION
 import json
 from pyrevit import HOST_APP, script
+from clr import StrongBox
 import System
 from System import String
 from System.Collections.Generic import Dictionary, IDictionary
@@ -53,10 +54,12 @@ def SetSchemaMapData(schema, fieldName, element, dictionary):
     data = entity.Get[IDictionary[String, String]](field)
     return data
 
+
 def GetSchemaMapData(schema, fieldName, element):
     entity = element.GetEntity(schema)
     data = entity.Get[IDictionary[String, String]](schema.GetField(fieldName))
     return data
+
 
 def GetFlamingoSchema(doc=None):
     doc = doc or HOST_APP.doc
@@ -80,25 +83,34 @@ def GetFlamingoSchema(doc=None):
     doc.ProjectInformation.SetEntity(entity)
     return flamingoSchema
 
-def GetFlamingoSetting(settingKey, default=None, doc=None):
-    doc = doc or HOST_APP.doc
-    schema = GetFlamingoSchema(doc)
-    projectInfo = doc.ProjectInformation
-    entity = projectInfo.GetEntity(schema)
-    settings = entity.Get[IDictionary[String, String]](schema.GetField("Settings"))
-    if settingKey in settings:
-        return settings[settingKey]
-    else:
-        return default
 
-def SetFlamingoSetting(settingKey, settingValue, doc=None):
+def GetFlamingoSetting(settingKey, default=None, doc=None, schema=None):
     doc = doc or HOST_APP.doc
-    schema = GetFlamingoSchema(doc)
+    schema = schema or GetFlamingoSchema(doc)
+    projectInfo = doc.ProjectInformation
+    settings = GetSchemaMapData(schema, "Settings", projectInfo)
+    LOGGER.debug("Current settings: {}".format(settings))
+    value = StrongBox[String]()
+    if settings.TryGetValue(String(settingKey), value):
+        return str(value.Value)
+    LOGGER.debug("Setting '{}' not found, returning default.".format(settingKey))
+    return default
+
+
+def SetFlamingoSetting(settingKey, settingValue, doc=None, schema=None):
+    doc = doc or HOST_APP.doc
+    schema = schema or GetFlamingoSchema(doc)
     projectInfo = doc.ProjectInformation
     entity = projectInfo.GetEntity(schema)
-    settings = entity.Get[IDictionary[String, String]](schema.GetField("Settings"))
-    if settings is None:
+    LOGGER.debug("Checking for existing settings...")
+    if entity.RecognizedField(schema.GetField("Settings")):
+        settings = entity.Get[IDictionary[String, String]](schema.GetField("Settings"))
+        LOGGER.debug("Existing settings found. settings: {}".format(settings))
+    else:
+        LOGGER.debug("No existing settings found. Creating new settings dictionary...")
         settings = Dictionary[String, String]()
-    settings[settingKey] = settingValue
+    settings[String(settingKey)] = String(settingValue)
+    LOGGER.debug("settings: {}".format(settings))
     entity.Set[IDictionary[String, String]](schema.GetField("Settings"), settings)
+    LOGGER.debug("Setting entity to project info")
     projectInfo.SetEntity(entity)
